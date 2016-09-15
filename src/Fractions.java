64cd71486705df4ee13e8f359f6454032cfc37ee
Vesa Nieminen <vesa.nieminen@gmail.com>
2016-09-15 22:10:56 +0300

    Initial commit.

---
+++ src/Fractions.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+
+public class Fractions {
+
+    enum Accuracy {
+        Halves(2),
+        Quarters(4),
+        Eights(8),
+        Sixteens(16);
+        int acc;
+        Accuracy(int acc) {
+            this.acc = acc;
+        }
+    }
+
+    public static String calculateFractionOf(BigDecimal number, Accuracy accuracy) {
+        BigDecimal integer = number.divideToIntegralValue(new BigDecimal(1));
+        double decimal = number.subtract(integer).doubleValue();
+        StringBuilder builder = new StringBuilder();
+        decimal = roundTo(decimal, accuracy.acc);
+        int numerator = integer.intValue() * accuracy.acc + (int)decimal;
+        if (numerator > 0) {
+            builder.append(numerator + "/" + accuracy.acc);
+        }
+        return builder.toString();
+    }
+
+    private static double roundTo(double decimal, double i) {
+        return Math.round(decimal * i) ;
+    }
+
+}
---
+++ src/TestFractions.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class TestFractions {
+    @Test
+    public void testHalves() {
+        Fractions.Accuracy half = Fractions.Accuracy.Halves;
+        Assert.assertEquals("2/2", Fractions.calculateFractionOf(new BigDecimal(1.1), half));
+        Assert.assertEquals("1/2", Fractions.calculateFractionOf(new BigDecimal(.5), half));
+        Assert.assertEquals("", Fractions.calculateFractionOf(new BigDecimal(0), half));
+        Assert.assertEquals("10/2", Fractions.calculateFractionOf(new BigDecimal(5), half));
+    }
+
+    @Test
+    public void testQuarters() {
+        Fractions.Accuracy quarters = Fractions.Accuracy.Quarters;
+        Assert.assertEquals("1/4", Fractions.calculateFractionOf(new BigDecimal(.25), quarters));
+        Assert.assertEquals("2/4", Fractions.calculateFractionOf(new BigDecimal(.5), quarters));
+        Assert.assertEquals("3/4", Fractions.calculateFractionOf(new BigDecimal(.8), quarters));
+        Assert.assertEquals("4/4", Fractions.calculateFractionOf(new BigDecimal(.9), quarters));
+        Assert.assertEquals("", Fractions.calculateFractionOf(new BigDecimal(0), quarters));
+    }
+}
